9e72d76a9c22761fd0799ef11e9b573ff8d5e3e1
LegendSave <96206372+LegendSave@users.noreply.github.com>
2022-11-15 12:41:50 -0800

    Create web.js

---
+++ web.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const slideApp = () => {
+  return (
+    <View style={{ flex: 1, justifyContent:"center",alignItems:"center" }}>
+    <Text>
+    This is some sample text
+    </Text>
+</View>
+);
+}
+
+export default slideApp;
